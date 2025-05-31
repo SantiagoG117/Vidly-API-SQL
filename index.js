1f821cddca9ca87195ad47e8b6835ca21bee8609
SantiagoG117 <garc0254@algonquinlive.com>
2025-05-31 19:57:48 -0400

    Build UI for user registration

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 const express = require("express");
 const app = express();
 
-/* 
-    TODO:
-    ! Build front end
-*/
-
-//? Export extrnal libraries
+//? Export external libraries
 require("dotenv").config();
+const cors = require("cors");
 
 //? Import routers
 const genres = require("./routes/genres");
@@ -20,6 +16,10 @@ const logins = require("./routes/login");
 const error = require("./middleware/errorHandler");
 
 //? Register middleware functions and routers
+// Add required CORS headers in the response. Must be declared first
+app.use(
+  cors({ origin: "http://127.0.0.1:5500", exposedHeaders: ["x-auth-token"] })
+);
 app.use(express.json()); //Takes requests and parse their body into a JSON object
 app.use("/api/genres", genres);
 app.use("/api/movies", movies);
@@ -27,6 +27,9 @@ app.use("/api/customers", customers);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/login", logins);
+// Serve HTML and JS files
+app.use(express.static("public"));
+
 //Error middleware: Must be declared at the end so route handlers can pass control to it in case of errors
 app.use(error);
 
